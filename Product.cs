302b533dc7435754a18667d41a8d42c6a1bea665
清风 <yao-lifeng@163.com>
2023-08-09 17:50:07 +0800

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// 商品抽象类
+    /// </summary>
+    public abstract class Product
+    {
+        /// <summary>
+        /// 商品名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 商品价格
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// 商品数量
+        /// </summary>
+        public int Num { get; set; }
+
+        /// <summary>
+        /// 是否进口商品
+        /// </summary>
+        public bool IsImported { get; set; }
+
+        private int ImportTaxRate = 5; // 进口税率
+
+        /// <summary>
+        /// 计算商品价格= 数量* 单价.
+        /// </summary>
+        /// <returns></returns>
+        public decimal CalculateProductPrice()
+        {
+            return Price * Num;
+        }
+
+        /// <summary>
+        /// 计算商品总价格= 数量* 单价+ 所有税费.
+        /// </summary>
+        /// <returns></returns>
+        public decimal CalculateProductTotalPrice()
+        {
+            var productPrice = CalculateProductPrice();
+            var totalPrice = productPrice + CalculateSalesTax();
+
+            // 进口商品
+            if (IsImported)
+            {
+                // 增加进口税
+                var importTax = Math.Round(productPrice * ImportTaxRate / 100 ,2, MidpointRounding.AwayFromZero);
+                totalPrice = totalPrice + importTax;
+            }
+
+            return totalPrice;
+        }
+
+        /// <summary>
+        /// 计算商品销售税=数量* 单价* 销售税率
+        /// </summary>
+        /// <returns></returns>
+        public abstract decimal CalculateSalesTax();
+    }
+
+
+    /// <summary>
+    /// 免税商品, 书籍,食品,医疗
+    /// </summary>
+    public class FreeTaxProduct : Product
+    {
+        public FreeTaxProduct(string name, decimal price, int num, bool isImported)
+        {
+            Name = name;
+            Price = price;
+            Num = num;
+            IsImported = isImported;
+        }
+
+        int salesTaxRate = 0; // 基本销售税;
+
+        public override decimal CalculateSalesTax()
+        {
+            return salesTaxRate;
+        }
+    }
+
+    /// <summary>
+    /// 一般商品,基本销售税10% 的附加税
+    /// </summary>
+    public class DefaultProduct : Product
+    {
+        public DefaultProduct(string name, decimal price, int num, bool isImported)
+        {
+            Name = name;
+            Price = price;
+            Num = num;
+            IsImported = isImported;
+        }
+
+        int salesTaxRate = 10; // 基本销售税;
+
+
+        public override decimal CalculateSalesTax()
+        {
+            return Math.Round(CalculateProductPrice() * salesTaxRate/100 ,2, MidpointRounding.AwayFromZero);
+        }
+    }
+}
---
+++ ShoppingCart.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// 购物车
+    /// </summary>
+    public class ShoppingCart
+    {
+        public List<Product> Products { get; }
+
+        public ShoppingCart()
+        {
+            Products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            Console.WriteLine($"{product.Num} {product.Name} at {string.Format("{0:0.00}", product.Price)}");
+            Products.Add(product);
+        }
+
+        /// <summary>
+        /// 计算商品原始价格
+        /// </summary>
+        /// <returns></returns>
+        public decimal CalculateTotalSalesTax()
+        {
+            decimal price = 0;
+            foreach (Product product in Products)
+            {
+                price += product.CalculateProductPrice();
+            }
+
+            return CalculateTotalPrice() - price;
+        }
+
+        /// <summary>
+        /// 计算总价格
+        /// </summary>
+        /// <returns></returns>
+        public decimal CalculateTotalPrice()
+        {
+            decimal totalPrice = 0;
+            foreach (var product in Products)
+            {
+                totalPrice += product.CalculateProductTotalPrice();
+            }
+
+            return totalPrice;
+        }
+
+        /// <summary>
+        /// 打印凭据
+        /// </summary>
+        public void PrintReceipt()
+        {
+            foreach (var product in Products)
+            {
+                Console.WriteLine($"{product.Num} {product.Name} at {string.Format("{0:0.00}", product.CalculateProductTotalPrice())}");
+            }
+
+            Console.WriteLine($"Sales Taxes:{string.Format("{0:0.00}", CalculateTotalSalesTax())}");
+            Console.WriteLine($"Total:{string.Format("{0:0.00}", CalculateTotalPrice())}");
+        }
+    }
+}
